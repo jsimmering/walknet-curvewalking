acace05cc34a4c98e465674ec4b2938551536097
jsimmering <jsimmering@techfak.uni-bielefeld.de>
2020-12-13 21:32:26 +0100

    work on inverse kinematic

--- src/walknet_curvewalking/controller/single_leg_controller.py
+++ src/walknet_curvewalking/controller/single_leg_controller.py
@@ -33,46 +33,49 @@ class SingleLegController:
     def kinematic_callback(self, data):
         rospy.loginfo(rospy.get_caller_id() + "kinematic_callback heard that %s", data)
         if data:
-            # pos = self.leg.body_c1_transform(
-            #    self.leg.alpha_forward_kinematics(self.leg.beta_forward_kinematics([0, 0, 0.13, 1])))
-            # pos = self.leg.body_c1_transform(self.leg.alpha_forward_kinematics(self.leg.beta_forward_kinematics()))
-            # tf_pos = self.leg.body_c1_transform()
-            # pos = self.leg.body_c1_transformation(0)
-            # rospy.loginfo("tf c1 pos: " + str(tf_pos))
-            # rospy.loginfo("transformation matrix c1 pos: " + str(pos))
-            # tf_pos = self.leg.alpha_forward_kinematics()
-            # pos = self.leg.c1_thigh_transformation(0)
-            # rospy.loginfo("tf thigh pos: " + str(tf_pos))
-            # rospy.loginfo("transformation matrix thigh pos: " + str(pos))
-            # tf_pos = self.leg.beta_forward_kinematics()
-            # pos = self.leg.thigh_tibia_transformation(0)
-            # rospy.loginfo("tf tibia pos: " + str(tf_pos))
-            # rospy.loginfo("transformation matrix tibia pos: " + str(pos))
-            # pos = self.leg.tibia_ee_transformation()
-            # rospy.loginfo("transformation matrix ee pos: " + str(pos))
-            tf_pos = self.leg.body_c1_transform(self.leg.alpha_forward_kinematics())
-            pos = self.leg.body_c1_transformation(0.3, self.leg.c1_thigh_transformation(-1.0))
-            rospy.loginfo("tf                    thigh pos in body frame: " + str(tf_pos))
-            rospy.loginfo("transformation matrix thigh pos in body frame: " + str(pos))
-            tf_pos = self.leg.body_c1_transform(
-                self.leg.alpha_forward_kinematics(self.leg.beta_forward_kinematics()))
-            pos = self.leg.body_c1_transformation(0.3, self.leg.c1_thigh_transformation(-1.0,
-                self.leg.thigh_tibia_transformation(0.3)))
-            rospy.loginfo("tf                    tibia pos in body frame: " + str(tf_pos))
-            rospy.loginfo("transformation matrix tibia pos in body frame: " + str(pos))
-            tf_pos = self.leg.body_c1_transform(
-                self.leg.alpha_forward_kinematics(self.leg.beta_forward_kinematics([0, 0, 0.13, 1])))
-            pos = self.leg.body_c1_transformation(0.3, self.leg.c1_thigh_transformation(-1.0,
-                self.leg.thigh_tibia_transformation(0.3, self.leg.tibia_ee_transformation())))
-            rospy.loginfo("tf                    ee pos in body frame: " + str(tf_pos))
+            # tf_pos = self.leg.body_c1_transform(self.leg.alpha_forward_kinematics())
+            # pos = self.leg.body_c1_transformation(0.3, self.leg.c1_thigh_transformation(-1.0))
+            # rospy.loginfo("tf                    thigh pos in body frame: " + str(tf_pos))
+            # rospy.loginfo("transformation matrix thigh pos in body frame: " + str(pos))
+            # tf_pos = self.leg.body_c1_transform(
+            #     self.leg.alpha_forward_kinematics(self.leg.beta_forward_kinematics()))
+            # pos = self.leg.body_c1_transformation(0.3, self.leg.c1_thigh_transformation(-1.0,
+            #     self.leg.thigh_tibia_transformation(0.3)))
+            # rospy.loginfo("tf                    tibia pos in body frame: " + str(tf_pos))
+            # rospy.loginfo("transformation matrix tibia pos in body frame: " + str(pos))
+            # tf_pos = self.leg.body_c1_transform(
+            #     self.leg.alpha_forward_kinematics(self.leg.beta_forward_kinematics([0, 0, 0.13, 1])))
+            # pos = self.leg.body_c1_transformation(0.3, self.leg.c1_thigh_transformation(-1.0,
+            #     self.leg.thigh_tibia_transformation(0.3, self.leg.tibia_ee_transformation())))
+            # rospy.loginfo("tf                    ee pos in body frame: " + str(tf_pos))
+            # rospy.loginfo("transformation matrix ee pos in body frame: " + str(pos))
+            # tf_pos = self.leg.compute_forward_kinematics_tf()
+            # pos = self.leg.compute_forward_kinematics()
+            # rospy.loginfo("tf                    ee pos in body frame: " + str(tf_pos))
+            # rospy.loginfo("transformation matrix ee pos in body frame: " + str(pos))
+            ##################################
+            cur_angles = self.leg.compute_inverse_kinematics()
+            rospy.loginfo('current alpha = ' + str(cur_angles[0]) +
+                          ' current beta = ' + str(cur_angles[1]) + ' current gamma = ' + str(cur_angles[2]))
+            ee_pos = self.leg.ee_position()
+            rospy.loginfo('current ee_pos = ' + str(ee_pos))
+            angles = self.leg.compute_inverse_kinematics(numpy.array([-0.1, 0.277, -0.11, 1]))
+            rospy.loginfo('angles to reach position [0.1, 0.277, -0.115, 1]: alpha = ' + str(angles[0]) +
+                          ' beta = ' + str(angles[1]) + ' gamma = ' + str(angles[2]))
+            self.leg.set_command(angles)
+            rate = rospy.Rate(10)
+            rate.sleep()
+            pos = self.leg.compute_forward_kinematics(angles)
             rospy.loginfo("transformation matrix ee pos in body frame: " + str(pos))
+            ee_pos = self.leg.ee_position()
+            rospy.loginfo('ee_pos = ' + str(ee_pos))
+
             tf_pos = self.leg.compute_forward_kinematics_tf()
             pos = self.leg.compute_forward_kinematics()
             rospy.loginfo("tf                    ee pos in body frame: " + str(tf_pos))
             rospy.loginfo("transformation matrix ee pos in body frame: " + str(pos))
-            # pos = self.leg.body_c1_transformation(0, self.leg.c1_thigh_transformation(0.3, self.leg.thigh_tibia_transformation(1, self.leg.tibia_ee_transformation())))
-            ##################################
         rospy.loginfo("end of callback")
+
 
     def manage_swing(self):
         rate = rospy.Rate(100)  # 100Hz
@@ -82,11 +85,12 @@ class SingleLegController:
         rospy.loginfo("leg connected start swing")
         swing = True
         self.swing_trajectory_gen.set_start_point = self.leg.ee_position()
-        self.swing_trajectory_gen.set_target_point = [0.1314795, 0.2563255, -0.1365]  # + TODO
+        self.swing_trajectory_gen.set_target_point = [0.1, 0.277, -0.11, 1]  # + TODO
         self.swing_trajectory_gen.compute_trajectory_points()
-        while not rospy.is_shutdown():
-            self.swing_trajectory_gen.move_to_next_point()
-            rate.sleep()  # swing_net.swing_start_point = numpy.array([0., 0., 0.])  # the point where the swing  #
+        rospy.loginfo('trajectory: ' + str(self.swing_trajectory_gen.trajectory))
+        #while not rospy.is_shutdown():
+        #    self.swing_trajectory_gen.move_to_next_point()
+        #    rate.sleep()  # swing_net.swing_start_point = numpy.array([0., 0., 0.])  # the point where the swing  #
             # phase starts  # swing_net.swing_target_point = numpy.array([1., 0., 0.])  # the point where the swing
             # phase should end  # at which position of the interval between the start and the end point the middle  #
             # point should be placed  # swing_net.apex_point_ratio = 0.5  # the offset that is added to the middle  #
@@ -97,29 +101,30 @@ class SingleLegController:
 
             # swing_net.move_to_next_point(0.5)
 
-    # def compute_segment_length(self):
-    # in meter
-    # c1_pos = [0, 0.1034, 0.001116]
-    # thigh_pos = [0, 0.1574, 0.001116]
-    # tibia_pos = [0.0001, 0.22275, -0.00885]
-    # ee_pos = [0.0015, 0.38631, -0.02038]
-    # c1_length = numpy.sqrt(
-    #    thigh_pos[0] - c1_pos[0] * thigh_pos[0] - c1_pos[0] + thigh_pos[1] - c1_pos[1] * thigh_pos[1] - c1_pos[1] +
-    #    thigh_pos[2] - c1_pos[2] * thigh_pos[2] - c1_pos[2])
-    # thigh_length = numpy.sqrt(
-    #    tibia_pos[0] - thigh_pos[0] * tibia_pos[0] - thigh_pos[0] + tibia_pos[1] - thigh_pos[1] * tibia_pos[1] -
-    #    thigh_pos[1] + tibia_pos[2] - thigh_pos[2] * tibia_pos[2] - thigh_pos[2])
-    # tibia_length = numpy.sqrt(
-    #    ee_pos[0] - tibia_pos[0] * ee_pos[0] - tibia_pos[0] + ee_pos[1] - tibia_pos[1] * ee_pos[1] - tibia_pos[1] +
-    #    ee_pos[2] - tibia_pos[2] * ee_pos[2] - tibia_pos[2])
-    # return [c1_length, thigh_length, tibia_length]
+
+# def compute_segment_length(self):
+# in meter
+# c1_pos = [0, 0.1034, 0.001116]
+# thigh_pos = [0, 0.1574, 0.001116]
+# tibia_pos = [0.0001, 0.22275, -0.00885]
+# ee_pos = [0.0015, 0.38631, -0.02038]
+# c1_length = numpy.sqrt(
+#    thigh_pos[0] - c1_pos[0] * thigh_pos[0] - c1_pos[0] + thigh_pos[1] - c1_pos[1] * thigh_pos[1] - c1_pos[1] +
+#    thigh_pos[2] - c1_pos[2] * thigh_pos[2] - c1_pos[2])
+# thigh_length = numpy.sqrt(
+#    tibia_pos[0] - thigh_pos[0] * tibia_pos[0] - thigh_pos[0] + tibia_pos[1] - thigh_pos[1] * tibia_pos[1] -
+#    thigh_pos[1] + tibia_pos[2] - thigh_pos[2] * tibia_pos[2] - thigh_pos[2])
+# tibia_length = numpy.sqrt(
+#    ee_pos[0] - tibia_pos[0] * ee_pos[0] - tibia_pos[0] + ee_pos[1] - tibia_pos[1] * ee_pos[1] - tibia_pos[1] +
+#    ee_pos[2] - tibia_pos[2] * ee_pos[2] - tibia_pos[2])
+# return [c1_length, thigh_length, tibia_length]
 
 
 if __name__ == '__main__':
     rospy.init_node('single_leg_controller', anonymous=True)
     legController = SingleLegController('lm')
-    rospy.spin()
-    # try:
-    # legController.manage_swing()
-    # except rospy.ROSInterruptException:
-    #    pass
+    #rospy.spin()
+    try:
+        legController.manage_swing()
+    except rospy.ROSInterruptException:
+        pass
--- src/walknet_curvewalking/phantomx/SingleLeg.py
+++ src/walknet_curvewalking/phantomx/SingleLeg.py
@@ -23,6 +23,10 @@ class SingleLeg:
         self.beta = None
         self.gamma = None
 
+        self.alpha_target = None
+        self.beta_target = None
+        self.gamma_target = None
+
         self.alpha_reached = True
         self.beta_reached = True
         self.gamma_reached = True
@@ -48,7 +52,8 @@ class SingleLeg:
     def c1_callback(self, data):
         # rospy.loginfo(rospy.get_caller_id() + "I heard that %s", data)
         self.alpha = data.process_value
-        if data.error < 0.05:
+        self.alpha_target = data.set_point
+        if data.error < 0.01:
             self.alpha_reached = True
         else:
             self.alpha_reached = False
@@ -56,7 +61,8 @@ class SingleLeg:
     def thigh_callback(self, data):
         # rospy.loginfo(rospy.get_caller_id() + "I heard that %s", data)
         self.beta = data.process_value
-        if data.error < 0.05:
+        self.beta_target = data.set_point
+        if data.error < 0.01:
             self.beta_reached = True
         else:
             self.beta_reached = False
@@ -64,7 +70,8 @@ class SingleLeg:
     def tibia_callback(self, data):
         # rospy.loginfo(rospy.get_caller_id() + "I heard that %s", data)
         self.gamma = data.process_value
-        if data.error < 0.05:
+        self.gamma_target = data.set_point
+        if data.error < 0.01:
             self.gamma_reached = True
         else:
             self.gamma_reached = False
@@ -75,6 +82,9 @@ class SingleLeg:
 
     def update_ee_position(self):
         self.ee_pos = self.compute_forward_kinematics()
+
+    def target_reached(self):
+        return self.alpha_reached and self.beta_reached and self.gamma_reached
 
     # compute ee_position based on current joint values in c1 coordinate frame (= leg coordinate frame)
     # code from https://www.programcreek.com/python/example/96799/tf.transformations
@@ -89,7 +99,11 @@ class SingleLeg:
         pos[1, 3] = trans[1]
         pos[2, 3] = trans[2]
         return numpy.array(numpy.dot(pos, [0, 0, 0.13, 1]))
-        #return numpy.array(numpy.dot(pos, [0, 0, 0, 1]))
+        # return numpy.array(numpy.dot(pos, [0, 0, 0, 1]))
+
+    def check_joint_ranges(self, angles):
+        return angles[0] >= -0.6 or angles[0] <= 0.6 or angles[1] >= -1.0 or angles[1] <= 0.3 or angles[2] >= -1.0 or \
+               angles[2] <= 1.0
 
     # ee position in body frame
     def compute_forward_kinematics(self, angles=None):
@@ -97,18 +111,34 @@ class SingleLeg:
             alpha = self.alpha
             beta = self.beta
             gamma = self.gamma
-        else:
+        elif self.check_joint_ranges(angles):
             alpha = angles[0]
             beta = angles[1]
             gamma = angles[2]
+        else:
+            raise Exception(
+                'The provided angles for ' + self.name + '(' + str(angles[0]) + ', ' + str(angles[1]) + ', ' + str(
+                    angles[2]) + ') are not valid for the forward/inverse kinematics.')
 
+        c1_pos = self.body_c1_transformation(alpha)
         temp_tarsus_position = self.body_c1_transformation(alpha, self.c1_thigh_transformation(beta,
             self.thigh_tibia_transformation(gamma, self.tibia_ee_transformation())))
-        # inverse kinematics alpha angle check
-        #alpha_check = -atan2(temp_tarsus_position[1], (temp_tarsus_position[0]))
-        #if abs(alpha_check - alpha) >= 0.01:
-        #    raise Exception('The provided angles for ' + self.name + '(' + str(alpha) + ', ' + str(beta) + ', ' + str(
-        #        gamma) + ') are not valid for the forward/inverse kinematics.')
+        # calculate shoulder angle as angle of vector from c1 pos to ee pos in body frame
+        # rospy.loginfo('temp_tarsus_position_y = ' + str(temp_tarsus_position[1]) + ' temp_tarsus_position_x =
+        #               ' + str(temp_tarsus_position[0]))
+        # rospy.loginfo(
+        #    'c1_pos_y = ' + str(c1_pos[1]) + ' c1_pos_x = ' + str(c1_pos[0]))
+        x_pos = temp_tarsus_position[0] - c1_pos[0]
+        y_pos = temp_tarsus_position[1] - c1_pos[1]
+        # TODO probably only works for lm leg find general solution!
+        alpha_check = -atan2(x_pos,
+            y_pos)  # switched x and y coordination because the leg points in the direction of the y axis of the
+        #             MP_BODY frame
+        # rospy.loginfo('ee_y = ' + str(y_pos) + ' ee_x = ' + str(x_pos) + ' alpha_check= ' + str(alpha_check) +
+        #               ' alpha = ' + str(alpha))
+        if abs(alpha_check - alpha) >= 0.01:
+            raise Exception('The provided angles for ' + self.name + '(' + str(alpha) + ', ' + str(beta) + ', ' + str(
+                gamma) + ') are not valid for the forward/inverse kinematics.')
         return temp_tarsus_position
 
     def body_c1_transformation(self, alpha, point=numpy.array([0, 0, 0, 1])):
@@ -122,6 +152,32 @@ class SingleLeg:
         trans = numpy.array([(cos90, sin_alpha * sin90, cos_alpha * sin90, 0),
             (0, cos_alpha, 0 - sin_alpha, 0.1034),
             (0 - sin90, sin_alpha * cos90, cos_alpha * cos90, 0.001116),
+            (0, 0, 0, 1)])
+        # print('trans: ', trans)
+        return trans.dot(point)
+
+    def body_leg_transformation(self, point=numpy.array([0, 0, 0, 1])):
+        # point=numpy.append(point,1)
+        # TODO ist alpha reversed?
+        # alpha *= -1  # The direction of alpha is reversed as compared to the denavit-hartenberg notation.
+        trans = numpy.array([(1, 0, 0, 0),
+            (0, 1, 0, 0.1034),
+            (0, 0, 1, 0.001116),
+            (0, 0, 0, 1)])
+        # print('trans: ', trans)
+        return trans.dot(point)
+
+    def leg_c1_transformation(self, alpha, point=numpy.array([0, 0, 0, 1])):
+        # point=numpy.append(point,1)
+        # TODO ist alpha reversed?
+        # alpha *= -1  # The direction of alpha is reversed as compared to the denavit-hartenberg notation.
+        cos_alpha = cos(alpha + radians(180))
+        sin_alpha = sin(alpha + radians(180))
+        cos90 = cos(radians(-90))
+        sin90 = sin(radians(-90))
+        trans = numpy.array([(cos90, sin_alpha * sin90, cos_alpha * sin90, 0),
+            (0, cos_alpha, 0 - sin_alpha, 0),
+            (0 - sin90, sin_alpha * cos90, cos_alpha * cos90, 0),
             (0, 0, 0, 1)])
         # print('trans: ', trans)
         return trans.dot(point)
@@ -200,17 +256,21 @@ class SingleLeg:
     #   When no position is provided the current position of the current leg is used
     #   to calculate the complementing angles.
     #   PhiPsi Inverse Transformation is applied (meaning the position is with respect
-    #   to the leg coordinate system aligned with the body coordinate system)
-    #   @param p point in leg coordinate system
+    #   to the leg coordinate system aligned with/equal to the body coordinate system)
+    #   @param p point in body coordinate system
     def compute_inverse_kinematics(self, p=None):
         if isinstance(p, (type(None))):
-            p = self.ee_pos
+            p = self.ee_position()
         if len(p) == 3:
             p = numpy.append(p, [1])
         p_temp = copy.copy(p)
         # p = self._phi_psi_trans_inv.dot(p)
-        alpha_angle = -atan2(p[1], (p[0]))
-        beta_pos = self.alpha_forward_kinematics(alpha_angle)
+        # alpha_angle: float = -atan2(p[1], (p[0]))
+        # TODO probably only works for lm leg find general solution!
+        c1_pos = self.body_c1_transformation(self.alpha)
+        alpha_angle = -atan2(p[0] - c1_pos[0], p[1] - c1_pos[1])  # switched x and y coordination because the leg points
+        #             in the direction of the y axis of the MP_BODY frame
+        beta_pos = self.body_c1_transformation(alpha_angle, self.c1_thigh_transformation(self.beta))  # 0 or self.beta?
         lct = numpy.linalg.norm(p[0:3] - beta_pos[0:3])
         # if (self.name=='rm'):
         #    print(p)
@@ -225,8 +285,8 @@ class SingleLeg:
                 h2 = pi
             else:
                 h2 = (acos((pow(lct, 2) + pow(self.segment_lengths[0], 2) - pow(numpy.linalg.norm(p[0:3]), 2)) / (
-                        2 * self.segment_lengths[
-                    0] * lct)))  # h2 = (acos((pow(lct, 2) + pow(self.segment_lengths[0], 2) - pow(  #   #   #   #  #
+                        2 * self.segment_lengths[0] * lct)))
+                # h2 = (acos((pow(lct, 2) + pow(self.segment_lengths[0], 2) - pow(  #   #   #   #  #
                 # numpy.linalg.norm(p[0:3]), 2)) /  #       (2 * self.segment_lengths[0] * lct)))
         except ValueError:
             raise ValueError('The provided position (' + str(p_temp[0]) + ', ' + str(p_temp[1]) + ', ' + str(
@@ -249,9 +309,9 @@ class SingleLeg:
         return [self.alpha, self.beta, self.gamma]
 
     def get_current_targets(self):
-        if self.alpha is None or self.beta is None or self.gamma is None:
+        if self.alpha_target is None or self.beta_target is None or self.gamma_target is None:
             return None
-        return [self.alpha.set_point, self.beta.set_point, self.gamma.set_point]
+        return [self.alpha_target, self.beta_target, self.gamma_target]
 
     def set_command(self, next_angles):
         # TODO check joint ranges
