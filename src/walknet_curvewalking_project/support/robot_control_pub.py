9b7fba2d4d7d77312f6fa920ba8a273c3a164832
jsimmering <jsimmering@techfak.uni-bielefeld.de>
2021-01-31 21:11:51 +0100

    arrived back at initial slow walking working state.

--- src/walknet_curvewalking_project/support/robot_control_pub.py
+++ src/walknet_curvewalking_project/support/robot_control_pub.py
@@ -5,7 +5,6 @@ from walknet_curvewalking.msg import robot_control
 
 
 def talker(speed_fact, pull_angle):
-    pub = rospy.Publisher('/control_robot', robot_control, queue_size=1)
     if not rospy.is_shutdown():
         msg = robot_control()
         msg.speed_fact = speed_fact
@@ -15,6 +14,7 @@ def talker(speed_fact, pull_angle):
 
 
 if __name__ == '__main__':
+    pub = rospy.Publisher('/control_robot', robot_control, queue_size=1)
     rospy.init_node('talker', anonymous=True)
     speed = rospy.get_param('~speed', 0.1)
     direction = rospy.get_param('~direction', 0.0)
