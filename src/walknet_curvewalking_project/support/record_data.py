13990eea0798bdbcbacf64aeb00bc5aeabdd38c2
jsimmering <jsimmering@techfak.uni-bielefeld.de>
2021-05-05 11:21:45 +0200

    allow for more fine tuned recording configuration of curve walk improvements

--- src/walknet_curvewalking_project/support/record_data.py
+++ src/walknet_curvewalking_project/support/record_data.py
@@ -14,86 +14,84 @@ def run(args):
 
 def main():
     repetitions_per_speed = 3
-    duration = 20
+    duration = 2
     circles = 1
     distance = None
 
-    step_length = True
-    shift_aep = True
-    decrease_inner_stance = True
+    step_length = [True]
+    shift_aep = [True]
+    decrease_inner_stance = [0.02]
 
     # direction = numpy.arange(0.10, 0.24, 0.05)
-    # direction = [0.3]
     # direction = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
-    direction = [0.5]
-    # speed = [0.02, 0.03, 0.04, 0.05, 0.06]
+    # direction = [1.1, 1.0, 0.9, 0.8, 0.7, 0.6, 0.5]
+    direction = [1.57]
     # speed = numpy.arange(0.02, 0.065, 0.01)
-    # 0.007,
-    # speed = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08]
-    # speed = [0.007]
+    # speed = [0.007, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08]
     speed = [0.05]
     for i in range(0, repetitions_per_speed):
-        for s in speed:
+        for j in range(0, len(decrease_inner_stance)):
             for d in direction:
-                print("#############################")
-                print("stand up")
-                stand_up_exit_code = None
-                for i in range(0, 5):
-                    stand_up_p = run(["rosrun", "walknet_curvewalking", "robot_controller.py", "_walk:=False"])
-                    try:
-                        stand_up_exit_code = stand_up_p.wait(10)
-                    except subprocess.TimeoutExpired:
-                        continue
-                    if stand_up_exit_code == 0:
-                        break
-                if stand_up_exit_code is None or stand_up_exit_code != 0:
-                    print("standing up failed!")
-                    exit(1)
+                for s in speed:
+                    print("#############################")
+                    print("stand up")
+                    stand_up_exit_code = None
+                    for k in range(0, 5):
+                        stand_up_p = run(["rosrun", "walknet_curvewalking", "robot_controller.py", "_walk:=False"])
+                        try:
+                            stand_up_exit_code = stand_up_p.wait(10)
+                        except subprocess.TimeoutExpired:
+                            continue
+                        if stand_up_exit_code == 0:
+                            break
+                    if stand_up_exit_code is None or stand_up_exit_code != 0:
+                        print("standing up failed!")
+                        exit(1)
 
-                print("reset sim")
-                sim_p = run(["rosservice", "call", "/gazebo/reset_simulation", "{}"])
-                sim_exit_code = sim_p.wait(10)
-                if sim_exit_code != 0:
-                    print("resetting simulation failed! This should not happen")
-                    exit(1)
+                    print("reset sim")
+                    sim_p = run(["rosservice", "call", "/gazebo/reset_simulation", "{}"])
+                    sim_exit_code = sim_p.wait(10)
+                    if sim_exit_code != 0:
+                        print("resetting simulation failed! This should not happen")
+                        exit(1)
 
-                print("initialize Robot")
-                if duration:
-                    print("recorder: Robot Collector walk for {} minutes".format(duration))
-                    robot_p = run(["rosrun", "walknet_curvewalking", "robot_controller.py", "_walk:=True",
-                                   "_stepLength:=" + str(step_length), "_aepShift:=" + str(shift_aep),
-                                   "_innerStep:=" + str(decrease_inner_stance),
-                                   "_duration:=" + str(duration)])
-                else:
-                    print("recorder: Robot Controller walk walk until stop")
-                    robot_p = run(["rosrun", "walknet_curvewalking", "robot_controller.py", "_walk:=True",
-                                   "_stepLength:=" + str(step_length), "_aepShift:=" + str(shift_aep),
-                                   "_innerStep:=" + str(decrease_inner_stance),
-                                   "_duration:=0"])
+                    print("initialize Robot")
+                    if duration:
+                        print("recorder: Robot Collector walk for {} minutes".format(duration))
+                        robot_p = run(["rosrun", "walknet_curvewalking", "robot_controller.py", "_walk:=True",
+                                       "_stepLength:=" + str(step_length[j]), "_aepShift:=" + str(shift_aep[j]),
+                                       "_innerStep:=" + str(decrease_inner_stance[j]),
+                                       "_duration:=" + str(duration)])
+                    else:
+                        print("recorder: Robot Controller walk walk until stop")
+                        robot_p = run(["rosrun", "walknet_curvewalking", "robot_controller.py", "_walk:=True",
+                                       "_stepLength:=" + str(step_length[j]), "_aepShift:=" + str(shift_aep[j]),
+                                       "_innerStep:=" + str(decrease_inner_stance[j]),
+                                       "_duration:=0"])
 
-                print("initialize Data Collector")
-                if circles:
-                    print("recorder: Data Collector walk {} circles".format(circles))
-                    data_collector_p = run(
-                            ["rosrun", "walknet_curvewalking", "DataCollector.py", "_circles:=" + str(circles),
-                             "_distance:=0"])
-                elif distance:
-                    print("recorder: Data Collector walk {} meter".format(distance))
-                    data_collector_p = run(
-                            ["rosrun", "walknet_curvewalking", "DataCollector.py", "_circles:=0",
-                             "_distance:=" + str(distance)])
-                else:
-                    print("recorder: Data Collector walk walk until stop")
-                    data_collector_p = run(
-                            ["rosrun", "walknet_curvewalking", "DataCollector.py", "_circles:=0", "_distance:=0.0"])
+                    print("initialize Data Collector")
+                    if circles:
+                        print("recorder: Data Collector walk {} circles".format(circles))
+                        data_collector_p = run(
+                                ["rosrun", "walknet_curvewalking", "DataCollector.py", "_circles:=" + str(circles),
+                                 "_distance:=0"])
+                    elif distance:
+                        print("recorder: Data Collector walk {} meter".format(distance))
+                        data_collector_p = run(
+                                ["rosrun", "walknet_curvewalking", "DataCollector.py", "_circles:=0",
+                                 "_distance:=" + str(distance)])
+                    else:
+                        print("recorder: Data Collector walk walk until stop")
+                        data_collector_p = run(
+                                ["rosrun", "walknet_curvewalking", "DataCollector.py", "_circles:=0", "_distance:=0.0"])
 
-                time.sleep(1)
-                print("start walking")
-                run(["rosrun", "walknet_curvewalking", "robot_control_pub.py", "_speed:=" + str(s),
-                     "_direction:=" + str(d)])
+                    time.sleep(1)
+                    print("start walking")
+                    run(["rosrun", "walknet_curvewalking", "robot_control_pub.py", "_speed:=" + str(s),
+                         "_direction:=" + str(d)])
 
-                robot_p.wait()
-                data_collector_p.wait()
+                    robot_p.wait()
+                    data_collector_p.wait()
 
 
 if __name__ == "__main__":
